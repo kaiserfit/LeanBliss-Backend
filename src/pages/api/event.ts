4c9b3a8f4cb50ca41ee12db866c59fb86e4e724c
KaiserFit <service@kaiserfit.net>
2024-02-06 00:39:50 +0800

    Edits for GeoLocation API Tests

--- src/pages/api/event.ts
+++ src/pages/api/event.ts
@@ -15,10 +15,10 @@ interface IFacebookApiRequest extends NextApiRequest {
     // last_name: string;              //{4}
     // gender: string;                 //{5}
     // birthday: string;               //{6}
-    // city: string;                   //{7}
-    // state: string;                  //{8}
-    // zip_code: string;               //{9}
-    // country: string;                //{10}
+    city: string;                   //{7}
+    state: string;                  //{8}
+    zip_code: string;               //{9}
+    country: string;                //{10}
     external_id: string;            //{11} | *This is userID
     client_ip_address: string;      //{12}
     client_user_agent: string;      //{13}
@@ -72,6 +72,10 @@ export default async function Handler(
               // st: sha256(req.body.state),                    //{8}
               // zp: sha256(req.body.zip_code),                 //{9}
               // country: sha256(req.body.country),             //{10}
+              ct: req.body.city,                     //{7}
+              st: req.body.state,                    //{8}
+              zp: req.body.zip_code,                 //{9}
+              country: req.body.country,             //{10}
               external_id: req.body.external_id,             //{11}
               client_ip_address: req.body.client_ip_address, //{12}
               client_user_agent: req.body.client_user_agent, //{13}
