8749926b38b84a7b7802d7f021848ca0f428892b
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2024-02-09 00:58:31 +0800

    Update next.config.mjs - added CORS

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+const withCors = require('nextjs-cors');
+
+module.exports = withCors({
+  // Your Next.js configuration options go here
+  // For example:
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Accept' },
+        ],
+      },
+    ];
+  },
+});
+
 export default nextConfig;
