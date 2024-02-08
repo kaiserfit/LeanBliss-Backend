8209414394655cc1a115b86fe5aebf6600d69654
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2024-02-09 01:30:53 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,27 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-import NextCors from 'nextjs-cors';
-
-export default {
-  reactStrictMode: true, // Enable strict mode for React
-  // Your other Next.js configuration options go here
-  async headers() {
-    return [
-      {
-        source: 'src/pages/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Accept' },
-        ],
-      },
-    ];
-  },
-  async serverMiddleware() {
-    // Enable CORS for all routes
-    this.nuxt.options.serverMiddleware.push(NextCors());
-  },
-};
-
 export default nextConfig;
